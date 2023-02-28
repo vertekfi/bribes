7628334eb5ca450ca37af68c0b41bb1b639ff193
0xBriz <chaindevone@gmail.com>
2023-02-28 06:01:33 -0500

    new deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,35 +5,35 @@ const INITIAL_TOKENS = [];
 const INITIAL_GAUGES = [];
 
 async function main() {
-  // const MerkleOrchard = await ethers.getContractFactory('MerkleOrchard');
-  // const rewardHandler = await upgrades.deployProxy(MerkleOrchard, []);
-  // await rewardHandler.deployed();
+  const MerkleOrchard = await ethers.getContractFactory('MerkleOrchard');
+  const rewardHandler = await upgrades.deployProxy(MerkleOrchard, []);
+  await rewardHandler.deployed();
 
-  // console.log(`MerkelOrchard deployed to: ${rewardHandler.address}`);
-
-  // const BribeManager = await ethers.getContractFactory('BribeManager');
-  // const bribeManager = await upgrades.deployProxy(BribeManager, [
-  //   GAUGE_CONTROLLER,
-  //   rewardHandler.address,
-  //   INITIAL_GAUGES,
-  //   INITIAL_TOKENS,
-  // ]);
-  // await bribeManager.deployed();
-
-  // console.log(`BribeManager deployed to: ${bribeManager.address}`);
-
-  // await rewardHandler.setBribeManager(bribeManager.address);
+  console.log(`MerkelOrchard deployed to: ${rewardHandler.address}`);
 
   const BribeManager = await ethers.getContractFactory('BribeManager');
-  const bribeManager = await upgrades.upgradeProxy(
-    '0xe1f6A52798b54D07E0Bed1d16e23897E0B336FDD',
-    BribeManager
-  );
+  const bribeManager = await upgrades.deployProxy(BribeManager, [
+    GAUGE_CONTROLLER,
+    rewardHandler.address,
+    INITIAL_GAUGES,
+    INITIAL_TOKENS,
+  ]);
   await bribeManager.deployed();
-  console.log(`BribeManager complete`);
+
+  console.log(`BribeManager deployed to: ${bribeManager.address}`);
+
+  await rewardHandler.setBribeManager(bribeManager.address);
+
+  // const BribeManager = await ethers.getContractFactory('BribeManager');
+  // const bribeManager = await upgrades.upgradeProxy(
+  //   '',
+  //   BribeManager
+  // );
+  // await bribeManager.deployed();
+  // console.log(`BribeManager complete`);
 
   // const MerkleOrchard = await ethers.getContractFactory('MerkleOrchard');
-  // const rewardHandler = await upgrades.upgradeProxy('0xf774745E9aDb7f82c461b04bef02AF1E00043386', MerkleOrchard);
+  // const rewardHandler = await upgrades.upgradeProxy('', MerkleOrchard);
   // await rewardHandler.deployed();
   // console.log(`MerkelOrchardupgrade complete`);
 }
